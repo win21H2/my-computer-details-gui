1c06ea57f9e264a0bace94d04dde9b13bc6e00f9
324Hz <1024Bitcoins@gmail.com>
2022-10-13 05:48:42 -0500

    Add project files.

---
+++ my-computer-details-gui/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace my_computer_details_gui.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ my-computer-details-gui/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using my_computer_details_gui.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace my_computer_details_gui.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
